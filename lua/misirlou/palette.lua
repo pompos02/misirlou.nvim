47843a3d13a11166db0114c2a023b92d720a9425
pompos02 <yiannis.caravellas@gmail.com>
2025-10-14 12:51:39 +0300

    style: more muted colors

--- lua/misirlou/palette.lua
+++ lua/misirlou/palette.lua
@@ -1,24 +1,26 @@
 local options = require("misirlou.config").options
 
 local palette = {
+
     bg = "#000000",
     nc = "#000000",
-    base = "#0e0e0e",
-    surface = "#1c1c24",
-    overlay = "#313131",
-    muted = "#4b4b4b",
-    subtle = "#5d5d5d",
-    text = "#c6c6c6",
-    red = "#85555f",
-    gold = "#94775f",
-    rose = "#916e6e",
-    blue = "#4c5764",
-    lavender = "#656580",
-    purple = "#776579",
-    green = "#637370",
-    highlight_low = "#262626",
-    highlight_med = "#4f4f4f",
-    highlight_high = "#797979",
+    base = "#121212",
+    surface = "#1e1e1e",
+    overlay = "#2a2a2a",
+    muted = "#404040",
+    subtle = "#4a4a4a",
+    text = "#c0c0c0",
+    red = "#9a5f68",
+    gold = "#a27d63",
+    rose = "#9f6f6f",
+    blue = "#5a6472",
+    lavender = "#72728b",
+    purple = "#7f6a7f",
+    green = "#6b7d78",
+
+    highlight_low = "#1e1e1e",
+    highlight_med = "#282828",
+    highlight_high = "#707070",
 }
 
 if options.palette ~= nil and next(options.palette) then
